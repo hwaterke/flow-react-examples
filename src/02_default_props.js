f982fcf429fc8b6a3669318e4a51c036471149da
hwaterke <hwaterke@users.noreply.github.com>
2017-05-13 22:57:57 +0200

    Example with default props

--- src/02_default_props.js
+++ src/02_default_props.js
@@ -1,12 +1,20 @@
 // @flow
 import React from 'react';
 
-// Define the props of your component
+type DefaultProps = {
+  name: string
+};
+
 type Props = {
+  name: string,
   age: number
 };
 
 class A extends React.Component {
+  static defaultProps = {
+    name: 'John'
+  };
+
   props: Props;
 
   // ...
@@ -16,7 +24,11 @@ class A extends React.Component {
 // Flow OK
 // --------------------
 
+// Default props do not have to be passed
 <A age={42} />;
+
+// But they can be
+<A age={42} name="Harold" />;
 
 // Flow will not complain for extra props that were not defined
 <A age={42} bar="ss" />;
@@ -25,8 +37,9 @@ class A extends React.Component {
 // Flow ERROR
 // --------------------
 
-// Flow report error for missing props
+// Flow report error for missing props (age)
 // <A />;
 
 // Flow report error for wrong type of props
 // <A age="hello" />;
+// <A age={42} name={33} />;
